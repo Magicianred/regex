2974b925c5c8b5f4b90b22ae14d1eb326b76eab7
Your Name <you@example.com>
2020-12-27 18:17:11 -0300

    ...

--- caracteres/AlgunsCuidados.js
+++ caracteres/AlgunsCuidados.js
@@ -1,1 +1,9 @@
-const textoUmaLinha = '...'
+const textoUmaLinha = '...' // aspas simples ou duplas
+
+const textoMultiLinha = `
+	linha 1
+	linha 2
+`
+
+// Cuidado com o tab!
+console.log('	'.match(/\s/g))
